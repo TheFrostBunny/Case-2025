c5e0656b4062a2827413cb91746ffbafeed5e418
TheFrostBunny <David.aa.leren@gmail.com>
2025-12-01 11:11:16 +0100

    lagt til Web-port i vite fra .env

--- Web/vite.config.ts
+++ Web/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load .env from parent folder
+  const env = loadEnv(mode, '../', '');
+  const port = env.VITE_Web_PORT ? Number(env.VITE_Web_PORT) : 5173;
+  return {
+    plugins: [react()],
+    server: {
+      port,
+    },
+  };
+});
